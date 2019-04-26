5a12eec374afc004615b86acb55f0776c02e0ac5
toblessing <toblessing@163.com>
2019-04-27 00:04:08 +0800

    安装了iview，增加了它的依赖

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router';
 import App from './App.vue'
 import './plugins/iview.js'
+import store from './store'
+import router from './router'
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
+
+Vue.use(VueRouter);
+Vue.use(VueRouter);
+Vue.use(iView);
+
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  store,
+  router,
+  render: h => h(App)
 }).$mount('#app')
